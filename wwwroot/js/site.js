0fb34a1a7c05a4a7171c6ff2638dd3560625efb6
sahukarg <gobindags@gmail.com>
2024-01-05 18:27:52 +0530

    webfiles

---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+setInterval(function () {
+    var hostnameElement = document.getElementById("hostname");
+    hostnameElement.classList.toggle("flashing-js");
+}, 1000); // Adjust interval as needed
